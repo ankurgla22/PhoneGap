f28b1cc125493ebf6e0085397503e165f634c3c5
ankurgla22 <ankurgla22@gmail.com>
2017-02-25 02:28:56 +0900

    save arezzo file1s

--- www/js/index.js
+++ www/js/index.js
@@ -16,50 +16,34 @@
  * specific language governing permissions and limitations
  * under the License.
  */
- var app = {
-     // Application Constructor
-     initialize: function() {
-         this.bindEvents();
-     },
-     // Bind Event Listeners
-     //
-     // Bind any events that are required on startup. Common events are:
-     // 'load', 'deviceready', 'offline', and 'online'.
-     bindEvents: function() {
-         document.addEventListener('deviceready', this.onDeviceReady, false);
-     },
-     // deviceready Event Handler
-     //
-     // The scope of 'this' is the event. In order to call the 'receivedEvent'
-     // function, we must explicitly call 'app.receivedEvent(...);'
-     onDeviceReady: function() {
-        // app.receivedEvent('deviceready');
-        navigator.geolocation.getCurrentPosition(app.onSuccess, app.onError);
-     },
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-     onSuccess: function(position){
-         var longitude = position.coords.longitude;
-         var latitude = position.coords.latitude;
-         var latLong = new google.maps.LatLng(latitude, longitude);
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-         var mapOptions = {
-             center: latLong,
-             zoom: 13,
-             mapTypeId: google.maps.MapTypeId.ROADMAP
-         };
-
-         var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-         var marker = new google.maps.Marker({
-               position: latLong,
-               map: map,
-               title: 'my location'
-           });
-     },
-
-     onError: function(error){
-         alert("the code is " + error.code + ". \n" + "message: " + error.message);
-     },
- };
-
- app.initialize();
+        console.log('Received Event: ' + id);
+    }
+};
